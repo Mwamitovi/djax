fb4438095a4b8570847a5fc8f2da5b94a0d1c61f
Martin Matovu <matovu.synergy@gmail.com>
2019-07-29 20:44:56 +0300

    Add STATICFILES_DIRS and STATIC_ROOT

--- djax/settings.py
+++ djax/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# tuple, directs django to look for static assets
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+# "collectstatic" files shall end up here
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
