7c3cf7586225d74c35416ed5792322b149718b88
Martin Matovu <matovu.synergy@gmail.com>
2019-09-14 09:01:10 +0300

    Define Entity model (meant to handle beyond employees) and register it for tagging

--- djax/models.py
+++ djax/models.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
-from django.db import models
-import datetime
 import re
+import datetime
+from django.db import models
+from tagging.registry import register
 
 
 OFFICE_CHOICES = (
@@ -45,3 +46,31 @@ class TextPhoneField(models.TextField):
     
     def remove_formatting(self):
         return re.sub(ur'\D', u'', str(self)
+
+
+class Entity(models.Model):
+    """
+    This class is basically the "Person" class; 
+    however, it is called "Entity" to emphasize that 
+    it is intended to accommodate people, offices, organizational units, 
+    and possibly other areas.
+    """
+    active = models.BooleanField(required=False)
+    department = models.ForeignKey(Entity, required=False)
+    description = models.TextField(required=False)
+    email = TextEmailField(required=False)
+    extension = ExtensionField(required=False)
+    homepage = TextURLField(required=False)
+    image = models.FileField(required=False)
+    location = LocationField(required=False)
+    honors = models.TextField(required=False)
+    name = models.TextField()
+    post_nominals = models.TextField(required=False)
+    publish_externally = models.BooleanField(required=False)
+    reports_to = models.ForeignKey(Entity, required=False)
+    start_date = models.DateField(required=False)
+
+
+# register Entity model to use with the tagging application
+# tagging is intended atleast initially to locate areas of expertise
+register(Entity)
