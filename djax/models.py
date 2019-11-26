b6109e7d7bfe0c92ff9faf525751fb89a844aadb
Martin Matovu <matovu.synergy@gmail.com>
2019-11-26 08:02:09 +0300

    Update TextField(), class TextPhoneField() and class Entity()

--- djax/models.py
+++ djax/models.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from __future__ import unicode_literals
 import re
-import datetime
+from datetime import datetime
 from django.db import models
 from tagging.registry import register
 
@@ -16,15 +16,14 @@ OFFICE_CHOICES = (
 
 
 class Location(models.Model):
-    notes = models.TextField(required=False)
+    notes = models.TextField()
     office = models.CharField(max_length=2, choices=OFFICE_CHOICES, required=False)
-    postal_address = models.TextField(required=False)
-    room = models.TextField(required=False)
-    coordinates = GPSCoordinate(required=False)
+    postal_address = models.TextField()
+    room = models.TextField()
 
     
 class TextEmailField(models.EmailField):
-    entity = models.ForiegnKey('Entity')
+    entity = models.ForeignKey('Entity')
 
     def get_internal_type(self):
         return 'TextField'
@@ -36,17 +35,17 @@ class TextURLField(models.URLField):
 
 
 class TextPhoneField(models.TextField):
-    number = TextField()
-    description = TextField()
+    number = models.TextField()
+    description = models.TextField()
 
     def __eq__(self, other):
         try:
             return self.remove_formatting() == other.remove_formatting()
-        except:
+        except ValueError:
             return False
     
     def remove_formatting(self):
-        return re.sub(ur'\D', '', str(self)
+        return re.sub(r'\D', '', str(self))
 
 
 class Entity(models.Model):
@@ -57,27 +56,29 @@ class Entity(models.Model):
     and possibly other areas.
     """
     active = models.BooleanField(required=False)
-    department = models.ForeignKey(Entity, required=False)
-    description = models.TextField(required=False)
+    # if defined, department should be another 'Entity'
+    department = models.ForeignKey('Entity', required=False)
+    description = models.TextField()
     email = TextEmailField(required=False)
-    extension = ExtensionField(required=False)
+    # extension = ExtensionField(required=False)
     homepage = TextURLField(required=False)
     image = models.FileField(required=False)
-    location = LocationField(required=False)
-    honors = models.TextField(required=False)
+    location = models.ForeignKey(Location, required=False)
+    honors = models.TextField()
     name = models.TextField()
-    post_nominals = models.TextField(required=False)
+    post_nominals = models.TextField()
     publish_externally = models.BooleanField(required=False)
-    reports_to = models.ForeignKey(Entity, required=False)
+    # if defined, reports_to should be another 'Entity'
+    reports_to = models.ForeignKey('Entity', required=False)
     start_date = models.DateField(required=False)
 
 
 # register Entity model to use with the tagging application
-# tagging is intended atleast initially to locate areas of expertise
+# tagging is intended at least initially to locate areas of expertise
 register(Entity)
 
 
 class TextStatus(models.Model):
-    datetime = models.DateTimeField(default=datetime.now)
+    datetime = models.DateTimeField(default=datetime.now())
     entity = models.ForeignKey(Entity)
     text = models.TextField()
